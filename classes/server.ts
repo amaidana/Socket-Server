0c759d1aba14d2b50ea0b2f1f9b1310785fb826d
Alexis Maidana <amaidana@poseidon.net.ar>
2022-11-15 00:44:50 -0300

    Sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,51 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io'
+import http from 'http';
+
+import * as socket from '../sockets/socket';
 
 export default class Server {
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
 
-    constructor () {
+    private static _instance: Server;
+    private httServer: http.Server;
+
+    private constructor () {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httServer = new http.Server(this.app);
+        this.io = new socketIO.Server(this.httServer, {
+            cors: {
+                origin: true, 
+                credentials: true,
+            }
+        });
+
+        this.listenSockets();
+    }
+
+    public static get instance (): any   {
+        return this._instance || (this._instance = new this())
     }
 
     start (callback: Function) {
-        this.app.listen(this.port, callback());
+        this.httServer.listen(this.port, callback());
+    }
+
+    private listenSockets () {
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', client => {
+            console.log('Cliente conectado');
+
+            // Estar pendiente de los mensajes
+            socket.message(client, this.io);
+
+            // Desconectar 
+            socket.disconnect(client);
+        });
     }
 }
